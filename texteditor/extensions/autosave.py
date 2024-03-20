960609f7a207a18580ebc4c40c707eed5f765d2d
lebao3105 <bao12345yocoo@gmail.com>
2024-03-20 20:41:32 +0700

    Close #1

--- texteditor/extensions/autosave.py
+++ texteditor/extensions/autosave.py
@@ -1,36 +1,28 @@
+from libtextworker.general import CraftItems
+from pygubu.builder import Builder
+from texteditor import VIEWS_DIR
 from threading import Thread
 from tkinter import Misc, Toplevel
 from typing import Callable
-
-from pygubu.builder import Builder
-from libtextworker.general import CraftItems
-
-from texteditor import VIEWS_DIR
-
 from .generic import clrcall, global_settings
 
-enabled = global_settings.getkey("editor.autosave", "enable", False, True)
-time = global_settings.getkey("editor.autosave", "time", False, True)
+__all__ = ( "TOGGLE", "AutoSave", "AutoSaveConfig", "AUTOSV_DELAY" )
 
-if enabled not in global_settings.yes_values:
-    enabled = False
+TOGGLE: bool = global_settings.getkey("editor.autosave", "enable", False, True) in global_settings.yes_values
+AUTOSV_DELAY: int = global_settings.getkey("editor.autosave", "time", False, True)
 
-if not int(time):
-    time = 30
-
-TOGGLE: bool = bool(enabled)
-
+if not int(AUTOSV_DELAY):
+    AUTOSV_DELAY = 30
 
 class AutoSave:
     Editor: Misc
-    CurrDelay: int = time
+    CurrDelay: int = AUTOSV_DELAY
     SaveFunc: Callable
 
-    def Start(self, time_: int = time):
-        self.CurrDelay = time_
-        if not enabled:
-            return
-        self.TaskId = self.Editor.after(int(time_) * 1000, lambda: self.SaveFunc())
+    def Start(self, delay: int = AUTOSV_DELAY):
+        if not TOGGLE: return
+        self.CurrDelay = delay
+        self.TaskId = self.Editor.after(int(delay) * 1000, lambda: self.SaveFunc())
 
     def Stop(self):
         self.Editor.after_cancel(self.TaskId)
@@ -52,12 +44,10 @@ class AutoSave:
         Thread(target=self.CheckToggle, daemon=True).start()
 
 
-class AutoSaveConfig(Toplevel):
+class AutoSaveConfig:
     """
     Autosave window.
     """
-
-    isShown: bool = False
 
     timealiases = {
         _("30 seconds"): 30,
@@ -67,30 +57,33 @@ class AutoSaveConfig(Toplevel):
         _("10 minutes"): 600,
         _("15 minutes"): 900,
         _("20 minutes"): 1200,
-        _("30 minutes"): 1800,
+        _("30 minutes"): 1800
     }
 
-    def __init__(self, master: Misc | None = None):
-        super().__init__(master)
-        self.grab_release()
-        self.wm_title("AutoSave config")
-        self.wm_protocol("WM_DELETE_WINDOW", self.OnClose)
-        self.resizable(False, False)
+    def ShowWind(self, master: Misc | None = None):
+        """
+        Shows the thing this class supposed to show.
+        In older versions: this is placed under \__init__ method.
+        But as this uses Toplevel which automatically shows itself (seems intended),
+        the dialog creation has been moved here.
+        """
+        self.dialog = Toplevel(master)
+        self.dialog.wm_title("AutoSave config")
+        self.dialog.grab_set()
+        self.dialog.resizable(False, False)
 
         self.builder = Builder(_)
         self.builder.add_from_file(CraftItems(VIEWS_DIR, "autosave.ui"))
 
-        self.dialog = self.builder.get_object("frame", self)
-        self.combobox = self.builder.get_object("combobox1", self.dialog)
-        self.checkbtn = self.builder.get_object("checkbutton1", self.dialog)
+        frame = self.builder.get_object("frame", self.dialog)
+        self.combobox = self.builder.get_object("combobox1", frame)
+        self.checkbtn = self.builder.get_object("checkbutton1", frame)
         self.combobox["values"] = list(self.timealiases)
         self.combobox["state"] = "readonly"
 
-        clrcall.configure(self.dialog, True)
+        clrcall.configure(frame, childs_too=True)
+        clrcall.autocolor_run(frame)
         self.builder.connect_callbacks(self)
-
-        # Github issue #5. How tf I need to do this?
-        self.destroy()
 
     def do_the_task(self):
         choice = self.builder.get_variable("selected_time").get()
@@ -100,13 +93,5 @@ class AutoSaveConfig(Toplevel):
             if do_save:
                 global_settings.update()
 
-    def ShowWind(self):
-        if not self.isShown:
-            self.mainloop()
-            self.isShown = True
-        else:
-            self.focus()
-
-    def OnClose(self):
-        self.isShown = False
-        self.destroy()
+    def destroy(self):
+        self.dialog.destroy()
